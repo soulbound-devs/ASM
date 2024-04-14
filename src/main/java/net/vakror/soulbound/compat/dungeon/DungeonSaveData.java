a54a5178862c3c24827aa237429dbc8573a9eb5e
vaylor27 <godisagodlol@gmail.com>
2024-04-14 14:40:29 -0400

    fix duplicates appearing in dungeon levels file

--- src/main/java/net/vakror/soulbound/compat/dungeon/DungeonSaveData.java
+++ src/main/java/net/vakror/soulbound/compat/dungeon/DungeonSaveData.java
@@ -1,30 +1,23 @@
 package net.vakror.soulbound.compat.dungeon;
 
-import net.minecraft.core.RegistryAccess;
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.core.registries.Registries;
 import net.minecraft.nbt.*;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.saveddata.SavedData;
 import net.minecraft.world.level.storage.DimensionDataStorage;
-import net.minecraft.world.phys.Vec3;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.event.level.LevelEvent;
 import net.vakror.soulbound.SoulboundMod;
 import net.vakror.soulbound.compat.dungeon.blocks.custom.DungeonAccessBlock;
-import net.vakror.soulbound.compat.dungeon.dimension.Dimensions;
-import net.vakror.soulbound.compat.dungeon.registry.DungeonRegistry;
-import net.vakror.soulbound.compat.dungeon.setup.DungeonSetup;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 public class DungeonSaveData {
     public static class DungeonLevelsListSaveData extends SavedData {
-        public final List<ResourceLocation> loadedDungeonLevels = new ArrayList<>();
+        public final Set<ResourceLocation> loadedDungeonLevels = new LinkedHashSet<>();
         public static DungeonLevelsListSaveData INSTANCE = new DungeonLevelsListSaveData();
 
         @Override
