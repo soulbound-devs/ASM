2b2abe2b730054ed467f8498be454c342a03987c
vaylor27 <godisagodlol@gmail.com>
2024-04-15 17:24:50 -0400

    make TextureRenderLayer no longer abstract

--- src/main/java/net/vakror/soulbound/extension/wand/TextureRenderLayer.java
+++ src/main/java/net/vakror/soulbound/extension/wand/TextureRenderLayer.java
@@ -4,7 +4,9 @@ import com.google.common.collect.ImmutableList;
 import com.mojang.math.Transformation;
 import net.minecraft.client.renderer.block.model.BakedQuad;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.client.resources.model.Material;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.inventory.InventoryMenu;
 import net.vakror.soulbound.model.ModelUtils;
 import net.vakror.soulbound.model.wand.api.AbstractWandLayer;
 import net.vakror.soulbound.model.wand.api.LayerRenderData;
@@ -13,7 +15,7 @@ import org.jetbrains.annotations.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
-public abstract class TextureRenderLayer extends AbstractWandLayer {
+public class TextureRenderLayer extends AbstractWandLayer {
     private ResourceLocation[] textures;
 
     public TextureRenderLayer(ResourceLocation... textures) {
@@ -21,13 +23,17 @@ public abstract class TextureRenderLayer extends AbstractWandLayer {
     }
 
     @Override
-    public void render(ImmutableList.Builder<BakedQuad> quads, Transformation transformation, LayerRenderData data) {
+    public final void render(ImmutableList.Builder<BakedQuad> quads, Transformation transformation, LayerRenderData data) {
         List<TextureAtlasSprite> sprites = new ArrayList<>();
         getAdditionalTextures(sprites, data);
         ModelUtils.genQuads(sprites, quads, transformation);
     }
 
-    public abstract void getAdditionalTextures(List<TextureAtlasSprite> sprites, LayerRenderData data);
+    public void getAdditionalTextures(List<TextureAtlasSprite> sprites, LayerRenderData data) {
+        for (ResourceLocation texture : textures) {
+            sprites.add(data.spriteGetter().apply(new Material(InventoryMenu.BLOCK_ATLAS, texture)));
+        }
+    }
 
     @Override
     public @NotNull String getCacheKey() {
