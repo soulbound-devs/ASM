9fdfabea18648f5d6ab2f1c8968336415ed82919
vaylor27 <godisagodlol@gmail.com>
2024-04-14 20:15:55 -0400

    remove AbstractWandLayer#id

--- src/main/java/net/vakror/soulbound/model/wand/api/AbstractWandLayer.java
+++ src/main/java/net/vakror/soulbound/model/wand/api/AbstractWandLayer.java
@@ -3,18 +3,12 @@ package net.vakror.soulbound.model.wand.api;
 import com.google.common.collect.ImmutableList;
 import com.mojang.math.Transformation;
 import net.minecraft.client.renderer.block.model.BakedQuad;
-import net.minecraft.resources.ResourceLocation;
 import net.vakror.soulbound.model.wand.WandBakedModel;
 import org.jetbrains.annotations.NotNull;
 
 public abstract class AbstractWandLayer {
-    public final ResourceLocation id;
     public LayerData data;
     public WandBakedModel model;
-
-    public AbstractWandLayer(ResourceLocation id) {
-        this.id = id;
-    }
 
     public abstract void render(ImmutableList.Builder<BakedQuad> quads, Transformation transformation);
 
